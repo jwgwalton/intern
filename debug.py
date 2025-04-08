3496fab2cb3420d3c0bf329c5b84ea04582fd278
jwgwalton <jwgwalton@gmail.com>
2025-04-08 14:45:13 +0100

    Start of development of job_hunter agent

--- debug.py
+++ debug.py
@@ -5,11 +5,13 @@ from typing import Dict
 
 from PIL import Image as PILImage
 from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
 
 # Load the ANTHROPIC_API_KEY, we need to do this before we import intern
 load_dotenv()
 
 from src.intern import intern
+from src.job_finder import create_job_finder
 
 
 def display_graph(graph):
@@ -22,7 +24,6 @@ def display_graph(graph):
     img.show()
 
 
-display_graph(intern.get_graph())
 
 
 def stream_graph_updates(graph, user_input:str, config:Dict[str, str]):
@@ -39,8 +40,21 @@ thread_id = str(uuid.uuid4())
 
 while True:
     config = {"configurable": {"thread_id": thread_id}}
+    tool_choice = input("What tool do you wish to work with?; ['intern' 'job_hunter']: ")
+    if tool_choice.lower() == "job_hunter":
+        print("Selected job_hunter")
+        model =  ChatAnthropic(model="claude-3-5-haiku-20241022")
+        tool = create_job_finder(model)
+        display_graph(tool.get_graph())
+
+    elif tool_choice.lower() == "intern":
+        tool = intern
+        display_graph(tool.get_graph())
+    else:
+        print("Invalid choice, please try again. The options are: ['intern' 'job_hunter']")
+        continue
     user_input = input("User: ")
     if user_input.lower() in ["quit", "exit", "q"]:
         print("Goodbye!")
         break
-    stream_graph_updates(intern, user_input, config)
+    stream_graph_updates(tool, user_input, config)
